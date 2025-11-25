599b2489cd2f6fe7459084832235d6f65f926505
JoseV-001 <Leaphaz@gmail.com>
2025-11-25 19:55:48 -0300

    feat: Versão 01 - Implementação via Classe Concreta (Initial commit)

---
+++ src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+
+import model.entities.Product;
+import util.ProductPredicate;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.00));
+        list.add(new Product("HD Case", 80.90));
+
+        list.removeIf(new ProductPredicate());
+
+        for (Product p : list){
+            System.out.println(p);
+        }
+
+
+
+
+    }
+}
